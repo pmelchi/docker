ee4882bfae3247f802d56f9924f3d0414758a615
Pablo Melchi <pmelchi@users.noreply.github.com>
2020-05-11 05:39:23 -0500

    Fixed typos and network error

--- nextcloud-app/script/docker_run.sh
+++ nextcloud-app/script/docker_run.sh
@@ -3,17 +3,18 @@
 #Set your own nextcloud storage path
 NEXTCLOUD_STORAGE=/nextcloud
 
-docker network create -d overlay nextcloud-network
+docker network create -d overlay --attachable nextcloud-network
 
 #Secrets are only available inside a swarm (which can also be standalone)
+#Path to "${NEXTCLOUD_STORAGE}/mysql" must exist
 docker run -d \
     --name nextcloud-db \
     -e MYSQL_ROOT_PASSWORD_FILE=/run/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD \
     -e MYSQL_PASSWORD_FILE=/run/secrets/NEXTCLOUD_MYSQL_PASSWORD \
     -e MYSQL_DATABASE_FILE=/run/secrets/NEXTCLOUD_MYSQL_DATABASE \
     -e MYSQL_USER_FILE=/run/secrets/NEXTCLOUD_MYSQL_USER \
-    --mount type=bind,source={$NEXTCLOUD_STORAGE}/mysql,target=/var/lib/mysql \
-    --network=nextcloud-network
-    mariadb:tag \
+    --mount type=bind,source="${NEXTCLOUD_STORAGE}/mysql",target=/var/lib/mysql \
+    --network=nextcloud-network \
+    mariadb:latest \
     --transaction-isolation=READ-COMMITTED \
     --binlog-format=ROW
