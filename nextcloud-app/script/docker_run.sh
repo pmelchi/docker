690a1997d883ce34d5205cdd781ec84b1046637b
Pablo Melchi <pmelchi@users.noreply.github.com>
2020-05-11 05:20:48 -0500

    Initial version to initialize database

--- nextcloud-app/script/docker_run.sh
+++ nextcloud-app/script/docker_run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
- NEXTCLOUD_STORAGE=/
+#Set your own nextcloud storage path
+NEXTCLOUD_STORAGE=/nextcloud
 
-docker volume 
+docker network create -d overlay nextcloud-network
 
+#Secrets are only available inside a swarm (which can also be standalone)
 docker run -d \
     --name nextcloud-db \
-    -e MYSQL_ROOT_PASSWORD=my-secret-pw \
-    mariadb:tag
+    -e MYSQL_ROOT_PASSWORD_FILE=/run/secrets/NEXTCLOUD_MYSQL_ROOT_PASSWORD \
+    -e MYSQL_PASSWORD_FILE=/run/secrets/NEXTCLOUD_MYSQL_PASSWORD \
+    -e MYSQL_DATABASE_FILE=/run/secrets/NEXTCLOUD_MYSQL_DATABASE \
+    -e MYSQL_USER_FILE=/run/secrets/NEXTCLOUD_MYSQL_USER \
+    --mount type=bind,source={$NEXTCLOUD_STORAGE}/mysql,target=/var/lib/mysql \
+    --network=nextcloud-network
+    mariadb:tag \
+    --transaction-isolation=READ-COMMITTED \
+    --binlog-format=ROW
