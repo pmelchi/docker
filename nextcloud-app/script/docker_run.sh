3bb5ad61864b795b6471322737c6914855b0283c
Pablo Melchi <pmelchi@users.noreply.github.com>
2020-05-12 05:48:06 -0500

    Deployment without web-tier

--- nextcloud-app/script/docker_run.sh
+++ nextcloud-app/script/docker_run.sh
@@ -4,6 +4,7 @@
 NEXTCLOUD_STORAGE=/nextcloud
 
 docker network create -d overlay --attachable nextcloud-network
+docker network create -d overlay --attachable nextcloud-proxy-network
 
 #Path to "${NEXTCLOUD_STORAGE}/mysql" must exist
 #env.list file must be updated with your values
@@ -16,3 +17,13 @@ docker run -d \
     mariadb:latest \
     --transaction-isolation=READ-COMMITTED \
     --binlog-format=ROW
+
+#Running nextcloud app/fpm
+docker run -d \
+    --name nextcloud-app \
+    -e MYSQL_HOST=nextcloud-db \
+    --env-file=env.list \
+    --mount type=bind,source="${NEXTCLOUD_STORAGE}/html",target=/var/www/html \
+    --network=nextcloud-network \
+    --restart=unless-stopped \
+    nextcloud:fpm-alpine
